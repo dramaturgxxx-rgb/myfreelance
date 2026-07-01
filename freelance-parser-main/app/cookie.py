950c2a24e3a1443d11a9a85c9cda4be88b7d90b7
dramaturgxxx-rgb <dramaturgxxx@gmail.com>
2026-07-01 14:18:25 +0200

    Delete freelance-parser-main directory

--- freelance-parser-main/app/cookie.py
+++
@@ -1,7 +0,0 @@
-from http.cookiejar import MozillaCookieJar
-
-
-def load_cookies_mozilla(path: str) -> MozillaCookieJar:
-    cj = MozillaCookieJar(path)
-    cj.load(ignore_discard=True, ignore_expires=True)
-    return cj
